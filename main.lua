c8d56070d793304b060c177f6f16282aefe0d55e
Phil Hagelberg <phil@hagelb.org>
2018-04-20 19:47:01 -0700

    Install fennel-aware traceback.

--- main.lua
+++ main.lua
@@ -1,6 +1,7 @@
 -- bootstrap the compiler
 fennel = require("lib.fennel")
 table[("insert")](package[("loaders")], fennel[("searcher")])
+debug.traceback = fennel.traceback
 pp = function(x) print(require("lib.fennelview")(x)) end
 
 fennel.dofile("main.fnl")
