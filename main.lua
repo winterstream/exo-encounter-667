fa76dd8cc5585ceb0673c4a6db635371efdd50ac
Phil Hagelberg <phil@hagelb.org>
2018-04-23 20:02:09 -0700

    Hack fennel to get better line numbers.

--- main.lua
+++ main.lua
@@ -1,8 +1,8 @@
 -- bootstrap the compiler
 fennel = require("lib.fennel")
-table[("insert")](package[("loaders")], fennel[("searcher")])
-debug.traceback = fennel.traceback
+table.insert(package.loaders, fennel.make_searcher({correlate=true}))
+-- table.insert(package.loaders, fennel.searcher)
 pp = function(x) print(require("lib.fennelview")(x)) end
 lume = require("lib.lume")
 
-fennel.dofile("main.fnl")
+fennel.dofile("main.fnl", {correlate=true})
